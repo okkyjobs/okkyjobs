593e0898b3adf3ab78699e9d23c1dc8e2b1ce03d
hhjkhhbgkugjnbj <145316956+hhjkhhbgkugjnbj@users.noreply.github.com>
2024-06-24 20:07:06 +0900

    Delete okky-security directory

--- okky-security/src/main/java/com/study/security20240312yeonho/service/auth/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.study.security20240312yeonho.service.auth;
-
-import com.study.security20240312yeonho.web.dto.UsernameCheckReqDto;
-
-public interface AuthService {
-
-	public boolean checkUsername(UsernameCheckReqDto usernameCheckReqDto) throws Exception;
-	public boolean signup() throws Exception;
-}
